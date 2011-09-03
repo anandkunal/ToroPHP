39395a773a67a93fda1dc86632a9d2269334ea2c
Martin Bean <martin@mcbwebdesign.co.uk>
2011-09-03 22:20:13 +0200

    Altered path_info logic.

--- toro.php
+++ toro.php
@@ -47,7 +47,9 @@ class ToroApplication {
         ToroHook::fire('before_request');
     
         $request_method = strtolower($_SERVER['REQUEST_METHOD']);
-        $path_info = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
+        $path_info = '';
+        $path_info = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : $path_info;
+        $path_info = isset($_SERVER['ORIG_PATH_INFO']) ? $_SERVER['ORIG_PATH_INFO'] : $path_info;
         $discovered_handler = NULL;
         $regex_matches = array();
         $method_arguments = NULL;
