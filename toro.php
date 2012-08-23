4002fa783a04c8aade7e106df8c63771b42f5c9f
Jason Mooberry <jason@vimeo.com>
2012-08-23 14:14:21 -0400

    Using hash marks as delimiters to avoid regex special character collisions on pipes

--- toro.php
+++ toro.php
@@ -17,7 +17,7 @@ class Toro {
                 $pattern = str_replace(':string', '([a-zA-Z]+)', $pattern);
                 $pattern = str_replace(':number', '([0-9]+)', $pattern);
                 $pattern = str_replace(':alpha', '([a-zA-Z0-9-_]+)', $pattern);
-                if (preg_match('|^/?' . $pattern . '/?$|', $path_info, $matches)) {
+                if (preg_match('#^/?' . $pattern . '/?$#', $path_info, $matches)) {
                     $discovered_handler = $handler_name;
                     $regex_matches = $matches;
                     break;
