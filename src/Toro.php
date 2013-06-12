a44d136bb47084529b6941499b2cf4a2c83888d4
anothergituser <anothergituser@gmail.com>
2013-06-12 18:15:27 +0200

    Fixed regex for url reverse. Added tests for ToroUtil

--- src/Toro.php
+++ src/Toro.php
@@ -122,6 +122,10 @@ class ToroHook
 
 class ToroUtil
 {
+
+    /* only used for testing */
+    public static $routes = array();
+
     /* 
      * Tries to return the url for a handler if the handler exists.
      * If the route of this handler was defined using parameters
@@ -155,7 +159,7 @@ class ToroUtil
     public static function url_for($handler, $params = array())
     {
         $tokens = Toro::getTokens();
-        $routes = Toro::getRoutes();
+        $routes = count(self::$routes) > 0 ? self::$routes : Toro::getRoutes();
 
         foreach ($routes as $pattern => $handler_name) 
         {
@@ -165,12 +169,43 @@ class ToroUtil
                 /* convert the tokens like :string to regex like ([a-zA-Z]+) */
                 $pattern = strtr($pattern, $tokens);
                 /* find all the regex parameters in the route pattern */
-                preg_match('/\(.*\)/', $pattern, $regs);
-                /* 
-                 * replace all the regex parameters with the parameters received
-                 * to construct the url
+                preg_match_all('#\([^\(\)]+\)#', $pattern, $regs);
+                /*
+                 * Replaces all the regex patterns in the defined route for this handler
+                 * with their parameter counterparts
+                 * 
+                 * If the defined route for this handler is something like /hello/:alpha-:alpha
+                 * and the reverse route is required, having specified an array of parameters
+                 * like array("this", "that") the resulting url should be /hello/this-that
+                 * 
+                 * Now, :alpha get's transformed to ([a-zA-Z0-9-_]+)
+                 * and then it is replaced with the parameter in the array
+                 * first occurence of the regex is replaced with the first parameter
+                 * the second occurence with the second parameter and so on..
+                 *
+                 * Finally, the resulting string is returned if the original pattern
+                 * for the handler matches it.                
                  */
-                $url = str_replace($regs, $params, $pattern);
+
+                if (isset($regs[0]))
+                {
+                    $url = $pattern;
+                    for ($i=0; $i < count($regs[0]); $i++) { 
+                        $pat = $regs[0][$i];
+                        if (isset($params[$i]))
+                        {
+                            $par = $params[$i];
+                            $pos = strpos($url, $pat);
+                            if ($pos != false)
+                            {
+                                $url = substr_replace($url, $par, $pos, strlen($pat));
+                            }
+                        }
+
+                    }    
+                }
+                
+
                 /* 
                  * test that the route pattern matches the resulting url
                  * to validate that the correct number and type of parameters
