97ff9e5320f12bdea2b8916d764079e9da7ffce4
Jeff Finley <quickliketurtle@gmail.com>
2014-02-05 19:27:03 -0800

    first pass at allowing @method to be passed in route.

--- src/Toro.php
+++ src/Toro.php
@@ -19,7 +19,7 @@ class Toro
                 $path_info = (strpos($_SERVER['REQUEST_URI'], '?') > 0) ? strstr($_SERVER['REQUEST_URI'], '?', true) : $_SERVER['REQUEST_URI'];
             }
         }
-        
+
         $discovered_handler = null;
         $regex_matches = array();
 
@@ -40,6 +40,12 @@ class Toro
                     break;
                 }
             }
+        }
+
+        if (strpos($discovered_handler, '@')) {
+            $position = strpos($discovered_handler, '@');
+            $request_method = substr($discovered_handler, $position + 1);
+            $discovered_handler = substr($discovered_handler, 0, $position);
         }
 
         $result = null;
