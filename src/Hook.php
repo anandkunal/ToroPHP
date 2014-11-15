d90ea536710152c2e2efe9abcf49337a7ea4daef
Martin Bean <martin@martinbean.co.uk>
2014-11-15 18:19:57 +0000

    Add docblocks to methods

--- src/Hook.php
+++ src/Hook.php
@@ -3,34 +3,81 @@ namespace Toro;
 
 class Hook
 {
+    /**
+     * The Hook instance.
+     *
+     * @var \Toro\Hook
+     */
     private static $instance;
 
-    private $hooks = array();
+    /**
+     * The registered hooks.
+     *
+     * @var array
+     */
+    private $hooks = [];
 
-    private function __construct() {}
-    private function __clone() {}
-
-    public static function add($hook_name, $fn)
+    /**
+     * Prevent the class from being instantiated directly by making its constructor private.
+     *
+     * @return void
+     */
+    private function __construct()
     {
-        $instance = self::get_instance();
-        $instance->hooks[$hook_name][] = $fn;
+        //
     }
 
-    public static function fire($hook_name, $params = null)
+    /**
+     * Prevent the class from being cloned.
+     *
+     * @return void
+     */
+    private function __clone()
     {
-        $instance = self::get_instance();
-        if (isset($instance->hooks[$hook_name])) {
-            foreach ($instance->hooks[$hook_name] as $fn) {
+        //
+    }
+
+    /**
+     * Add a hook.
+     *
+     * @param  string    $name
+     * @param  callable  $fn
+     * @return void
+     */
+    public static function add($name, $fn)
+    {
+        (self::getInstance())->hooks[$name][] = $fn;
+    }
+
+    /**
+     * Fire hooks for the specified name.
+     *
+     * @param  string  $name 
+     * @param  string  $params 
+     * @return void
+     */
+    public static function fire($name, $params = null)
+    {
+        $instance = self::getInstance();
+
+        if (isset($instance->hooks[$name])) {
+            foreach ($instance->hooks[$name] as $fn) {
                 call_user_func_array($fn, array(&$params));
             }
         }
     }
 
-    public static function get_instance()
+    /**
+     * Get an instance of the Hook class.
+     *
+     * @return \Toro\Hook
+     */
+    public static function getInstance()
     {
         if (empty(self::$instance)) {
             self::$instance = new Hook();
         }
+
         return self::$instance;
     }
 }
--- src/Router.php
+++ src/Router.php
@@ -5,35 +5,39 @@ use Toro\Hook;
 
 class Router
 {
+    /**
+     * Serve the application.
+     *
+     * @param  array  $routes 
+     * @return void
+     */
     public static function serve($routes)
     {
         Hook::fire('before_request', compact('routes'));
 
         $request_method = strtolower($_SERVER['REQUEST_METHOD']);
         $path_info = '/';
-        if (!empty($_SERVER['PATH_INFO'])) {
+
+        if (! empty($_SERVER['PATH_INFO'])) {
             $path_info = $_SERVER['PATH_INFO'];
-        }
-        else if (!empty($_SERVER['ORIG_PATH_INFO']) && $_SERVER['ORIG_PATH_INFO'] !== '/index.php') {
+        } elseif (! empty($_SERVER['ORIG_PATH_INFO']) && $_SERVER['ORIG_PATH_INFO'] !== '/index.php') {
             $path_info = $_SERVER['ORIG_PATH_INFO'];
-        }
-        else {
-            if (!empty($_SERVER['REQUEST_URI'])) {
+        } else {
+            if (! empty($_SERVER['REQUEST_URI'])) {
                 $path_info = (strpos($_SERVER['REQUEST_URI'], '?') > 0) ? strstr($_SERVER['REQUEST_URI'], '?', true) : $_SERVER['REQUEST_URI'];
             }
         }
-        
+
         $discovered_handler = null;
-        $regex_matches = array();
+        $regex_matches = [];
 
         if (isset($routes[$path_info])) {
             $discovered_handler = $routes[$path_info];
-        }
-        else if ($routes) {
+        } elseif ($routes) {
             $tokens = array(
                 ':string' => '([a-zA-Z]+)',
                 ':number' => '([0-9]+)',
-                ':alpha'  => '([a-zA-Z0-9-_]+)'
+                ':alpha'  => '([a-zA-Z0-9-_]+)',
             );
             foreach ($routes as $pattern => $handler_name) {
                 $pattern = strtr($pattern, $tokens);
@@ -45,14 +49,12 @@ class Router
             }
         }
 
-        $result = null;
+        $result = $handler_instance = null;
 
-        $handler_instance = null;
         if ($discovered_handler) {
             if (is_string($discovered_handler)) {
                 $handler_instance = new $discovered_handler();
-            }
-            elseif (is_callable($discovered_handler)) {
+            } elseif (is_callable($discovered_handler)) {
                 $handler_instance = $discovered_handler();
             }
         }
@@ -60,7 +62,7 @@ class Router
         if ($handler_instance) {
             unset($regex_matches[0]);
 
-            if (self::is_xhr_request() && method_exists($handler_instance, $request_method . '_xhr')) {
+            if (self::isXhrRequest() && method_exists($handler_instance, $request_method . '_xhr')) {
                 header('Content-type: application/json');
                 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
                 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
@@ -74,19 +76,22 @@ class Router
                 Hook::fire('before_handler', compact('routes', 'discovered_handler', 'request_method', 'regex_matches'));
                 $result = call_user_func_array(array($handler_instance, $request_method), $regex_matches);
                 Hook::fire('after_handler', compact('routes', 'discovered_handler', 'request_method', 'regex_matches', 'result'));
-            }
-            else {
+            } else {
                 Hook::fire('404', compact('routes', 'discovered_handler', 'request_method', 'regex_matches'));
             }
-        }
-        else {
+        } else {
             Hook::fire('404', compact('routes', 'discovered_handler', 'request_method', 'regex_matches'));
         }
 
         Hook::fire('after_request', compact('routes', 'discovered_handler', 'request_method', 'regex_matches', 'result'));
     }
 
-    private static function is_xhr_request()
+    /**
+     * Whether this request came via AJAX.
+     *
+     * @return bool
+     */
+    protected static function isXhrRequest()
     {
         return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest';
     }
