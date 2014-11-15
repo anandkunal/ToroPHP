f9a6c67f6f8c8dc0f5972e07ad61a9f277bfeeb7
Martin Bean <martin@martinbean.co.uk>
2014-11-15 18:00:14 +0000

    Move ToroHook class into its own file

--- src/Router.php
+++ src/Router.php
@@ -1,5 +1,4 @@
 <?php
-
 class Toro
 {
     public static function serve($routes)
@@ -86,39 +85,5 @@ class Toro
     private static function is_xhr_request()
     {
         return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest';
-    }
-}
-
-class ToroHook
-{
-    private static $instance;
-
-    private $hooks = array();
-
-    private function __construct() {}
-    private function __clone() {}
-
-    public static function add($hook_name, $fn)
-    {
-        $instance = self::get_instance();
-        $instance->hooks[$hook_name][] = $fn;
-    }
-
-    public static function fire($hook_name, $params = null)
-    {
-        $instance = self::get_instance();
-        if (isset($instance->hooks[$hook_name])) {
-            foreach ($instance->hooks[$hook_name] as $fn) {
-                call_user_func_array($fn, array(&$params));
-            }
-        }
-    }
-
-    public static function get_instance()
-    {
-        if (empty(self::$instance)) {
-            self::$instance = new ToroHook();
-        }
-        return self::$instance;
     }
 }
---
+++ src/ToroHook.php
@@ -0,0 +1,34 @@
+<?php
+class ToroHook
+{
+    private static $instance;
+
+    private $hooks = array();
+
+    private function __construct() {}
+    private function __clone() {}
+
+    public static function add($hook_name, $fn)
+    {
+        $instance = self::get_instance();
+        $instance->hooks[$hook_name][] = $fn;
+    }
+
+    public static function fire($hook_name, $params = null)
+    {
+        $instance = self::get_instance();
+        if (isset($instance->hooks[$hook_name])) {
+            foreach ($instance->hooks[$hook_name] as $fn) {
+                call_user_func_array($fn, array(&$params));
+            }
+        }
+    }
+
+    public static function get_instance()
+    {
+        if (empty(self::$instance)) {
+            self::$instance = new ToroHook();
+        }
+        return self::$instance;
+    }
+}
